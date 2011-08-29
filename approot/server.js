b26c305ec7c2197c08ed83083ecad6cd225e8229
James Lawrence Turner <james@jameslawrenceturner.com>
2011-08-29 09:11:01 -0400

    Added dump exceptions for error handler

--- approot/server.js
+++ approot/server.js
@@ -118,7 +118,8 @@ var app = express.createServer();
 
 app.configure(function(){
 	app.use(express.bodyParser());
-	app.use(express.staticProvider(__dirname + '/public')); 
+	app.use(express.static(__dirname + '/public'));
+	app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
 });
 
 /*
